6465fe9720f743ea9fd7a5a69ea657cea9ffdec3
lalo1234321 <eduardodominguezdev@gmail.com>
2022-09-23 14:30:48 -0500

    end of the app

--- app/src/main/java/com/example/appclimakotlin/MainActivity.kt
+++ app/src/main/java/com/example/appclimakotlin/MainActivity.kt
@@ -2,13 +2,35 @@ package com.example.appclimakotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var tvGrados:TextView? = null
+    var tvCiudad:TextView? = null
+    var tvEstatus:TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvGrados = findViewById<TextView>(R.id.textView3)
+        tvCiudad = findViewById<TextView>(R.id.textView2)
+        tvEstatus = findViewById<TextView>(R.id.textView4)
         var ciudad = intent.getStringExtra("com.example.appclimakotlin.Ciudades")
-        Toast.makeText(this, ciudad, Toast.LENGTH_SHORT).show()
+        var ciudadMexico = Ciudad("Ciudad de México", 50, "Caluroso")
+        var ciudadBerlin = Ciudad("Ciudad de Berlin", 80, "Nublado")
+        //Toast.makeText(this, ciudad, Toast.LENGTH_SHORT).show()
+        if( ciudad == "ciudad-mexico" ) {
+            tvEstatus?.text = ciudadMexico.estatus
+            tvCiudad?.text = ciudadMexico.nombre
+            tvGrados?.text = ciudadMexico.grados.toString() + "°"
+        }
+        else if( ciudad == "ciudad-berlin" ) {
+            tvEstatus?.text = ciudadBerlin.estatus
+            tvCiudad?.text = ciudadBerlin.nombre
+            tvGrados?.text = ciudadBerlin.grados.toString() + "°"
+        }else {
+            Toast.makeText(this, "El dato no se encuentra", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
