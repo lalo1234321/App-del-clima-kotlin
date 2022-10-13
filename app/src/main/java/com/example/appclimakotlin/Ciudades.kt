fd217b00925d657d3c3c60c9fdce49b7c1d1f015
lalo1234321 <eduardodominguezdev@gmail.com>
2022-10-13 10:49:50 -0500

    adding function to user the volley library

--- app/src/main/java/com/example/appclimakotlin/Ciudades.kt
+++ app/src/main/java/com/example/appclimakotlin/Ciudades.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class Ciudades : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,7 @@ class Ciudades : AppCompatActivity() {
         setContentView(R.layout.activity_ciudades)
         var bMexico = findViewById<Button>(R.id.bMexico)
         var bBerlin = findViewById<Button>(R.id.bBerlin)
+        var bValidateNetWork = findViewById<Button>(R.id.bNetworkValidator)
         val TAG = "com.example.appclimakotlin.Ciudades"
         bMexico.setOnClickListener(View.OnClickListener {
             var intent = Intent(this, MainActivity::class.java)
@@ -24,5 +29,31 @@ class Ciudades : AppCompatActivity() {
             intent.putExtra(TAG, "ciudad-berlin")
             startActivity(intent)
         })
+
+        bValidateNetWork.setOnClickListener{
+            if( Network.validateNetwork(this) ) {
+                Toast.makeText(this, "Hay conexión", Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(this, "No hay red", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun httpRequestVolley( url:String ) {
+        //this library act like a queue, we can manage many http requests
+        var queue = Volley.newRequestQueue(this)
+        //StringRequests return a result in string format
+        var request = StringRequest(Request.Method.GET,url, Response.Listener<String>{
+                response ->
+            try {
+                //code to process request
+                println(response)
+            } catch ( e:Exception ){
+                print(e)
+            }
+        }, Response.ErrorListener{
+
+        })
+        queue.add(request)
     }
 }
